713d3ab8d43cd1a4ed3d8161ff6217f3ca1bdd61
vitaly.v <volkov.vitaly.v@gmail.com>
2019-02-18 01:40:12 +0300

    added visitors interfaces

---
+++ src/Infrastructure/Visitable.php
@@ -0,0 +1,14 @@
+<?php declare(strict_types=1);
+
+namespace Club\Infrastructure;
+
+/**
+ * Visitable component
+ */
+interface Visitable
+{
+    /**
+     * @param Visitor $visitor
+     */
+    public function accept(Visitor $visitor);
+}
---
+++ src/Infrastructure/Visitor.php
@@ -0,0 +1,34 @@
+<?php declare(strict_types=1);
+
+namespace Club\Infrastructure;
+
+use Club\Club\Club;
+use Club\MusicPlayer\MusicPlayer;
+use Club\Persons\Person;
+use Club\Persons\States\PersonState;
+
+/**
+ * Visitor for club components
+ */
+interface Visitor
+{
+    /**
+     * @param Club $club
+     */
+    public function visitClub(Club $club): void;
+
+    /**
+     * @param MusicPlayer $musicPlayer
+     */
+    public function visitMusicPlayer(MusicPlayer $musicPlayer): void;
+
+    /**
+     * @param Person $person
+     */
+    public function visitPerson(Person $person): void;
+
+    /**
+     * @param PersonState $personState
+     */
+    public function visitPersonState(PersonState $personState): void;
+}
